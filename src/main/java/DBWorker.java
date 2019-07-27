ba8584e42f9ee68cf84682e0b114f5705a69206b
RomanSulyma <roman.sulima777@gmail.com>
2019-07-27 11:27:54 +0300

    First release of NAU Schedule BOT

--- src/main/java/DBWorker.java
+++ src/main/java/DBWorker.java
@@ -2,17 +2,18 @@ import java.sql.*;
 import java.util.TimeZone;
 
 public class DBWorker {
-    private static final String USER = "root";
-    private final String PASS = "Romka132";
-    private static String url = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=" + TimeZone.getDefault().getID()+"&useSSL=false";
 
+    private static final String USER = "wtpbskgebkbagn";
+    private final String PASS = "e9939d6bc247e26f749d09d02a37e75970c6c39f09b67411b0a70cd8f11776fe";
+    private static String url = "jdbc:postgres://wtpbskgebkbagn:e9939d6bc247e26f749d09d02a37e75970c6c39f09b67411b0a70cd8f11776fe@ec2-54-217-234-157.eu-west-1.compute.amazonaws.com:5432/d8ra2j3ikmj92d";
+//jdbc:mysql://localhost:3306/mydbtest?serverTimezone=" + TimeZone.getDefault().getID()+"&useSSL=false
     private Connection conn;
     private Statement stat;
 
     public void connect()
     {
         try {
-            conn = DriverManager.getConnection(url,USER,PASS);
+            conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
             e.printStackTrace();
         }
